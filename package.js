b0872025bee59369f78ef13f90c20463df3eaba0
Yasar icli <hello@yasaricli.com>
2015-05-26 15:20:59 +0300

    first commit

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'yasaricli:dynamicaudio',
+  version: '0.0.1',
+  summary: "An interface for writing audio with Firefox's new audio data API with a Flash fallback for older browsers.",
+  git: 'https://github.com/yasaricli/meteor-dynamicaudio.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  // js files
+  api.addFiles([
+    'lib/swfobject.js',
+    'lib/dynamicaudio.js'
+  ], 'client');
+
+  // export
+  api.export(['DynamicAudio']);
+});
